5d27219f9bd62116bbbe3a0de8d5469487daa60d
Max Maximov <maxim.maximov@corp.mail.ru>
2024-07-22 17:43:29 +0300

    fix: slog.Default() as default logger on init, until rewrite

--- internal/mountain/logging/slog.go
+++ internal/mountain/logging/slog.go
@@ -2,48 +2,48 @@ package logging
 
 import (
 	"log/slog"
-	"lua-mountain/internal/mountain/repository"
-	"lua-mountain/pkg/slogan"
 	"os"
 	"strings"
-)
 
+	"lua-mountain/internal/mountain/repository"
+	"lua-mountain/pkg/slogan"
+)
 
 type (
 	Config struct {
-		Target string `yaml:"target"`
-		Format string `yaml:"format"`
-		Level slog.Level `yaml:"level"`
+		Target string     `yaml:"target"`
+		Format string     `yaml:"format"`
+		Level  slog.Level `yaml:"level"`
 	}
 )
 
 var (
-	DefaultLogger *slog.Logger
+	DefaultLogger = slog.Default()
 )
 
 func Init(cfg *Config) error {
 	var (
-		err error
+		err     error
 		handler slog.Handler
-		target *os.File
+		target  *os.File
 	)
 
 	if cfg.Target == "" {
 		target = os.Stdout
-	} else if target, err = os.Open(cfg.Target); err != nil  {
+	} else if target, err = os.Open(cfg.Target); err != nil {
 		return err
 	}
 
 	switch strings.ToLower(cfg.Format) {
 	case "json":
 		handler = slogan.NewJSONHandler(target, &slog.HandlerOptions{
-			Level:       cfg.Level,
+			Level: cfg.Level,
 		}, repository.RequestIdContextKey)
 	case "text":
 		fallthrough
 	default:
 		handler = slogan.NewTextHandler(target, &slog.HandlerOptions{
-			Level:       cfg.Level,
+			Level: cfg.Level,
 		}, repository.RequestIdContextKey)
 	}
 
@@ -51,4 +51,3 @@ func Init(cfg *Config) error {
 	slog.SetDefault(DefaultLogger)
 	return nil
 }
-
